729cd8f90f90e7e7da9bd279cb5de8610d8c3a71
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2020-01-31 13:51:05 -0300

    Publicar aplicação no Heroku

--- config/express.js
+++ config/express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var home = require('../app/routes/home');
 module.exports = function(){
 	var app = express();
-	//app.set('port', 8080);
+	//Mudança no parâmetro PORT para publicar no Heroku
 	app.set('port', process.env.PORT);
 	//Middleware
 	app.use(express.static('./public'));
